92a4c36042106057e53212c94067619a2dae327f
Franchis Chen <chenl47@cardiff.ac.uk>
2018-11-01 16:38:09 +0000

    using momeng and react-dates

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import ExpenseForm from './ExpenseForm';
 
 class AddExpensePage extends Component {
     render() {
         return (
             <div>
-                Add expense page
+                <h1>Add Expense</h1>
+                <ExpenseForm />
             </div>
         )
     }
 }
 
-export default AddExpensePage;
+const mapStateToMap = (state) => ({
+    expenses: state.expenses
+});
+
+export default connect(mapStateToMap)(AddExpensePage);
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+export default class ExpenseForm extends Component {
+    state = {
+        description: '',
+        note: '',
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
+    };
+
+    onDescriptionChange = (e) => {
+        const description = e.target.value;
+        this.setState(()=>({ description }));
+    };
+
+    onNoteChange = (e) => {
+      const note = e.target.value;
+      this.setState(()=>({ note }));
+    };
+
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+
+        // Using regular expression
+        if(amount.match(/^\d*(\.\d{0,2})?$/)) {
+            this.setState(()=>({ amount }));
+        }
+
+    };
+
+    onDateChange = (createdAt) => {
+        this.setState(()=>({createdAt}));
+    };
+
+    onFocusChange = ({focused}) => {
+        this.setState(()=>({calendarFocused:focused}));
+    };
+
+    render() {
+        return (
+            <form>
+                <input
+                    type="text"
+                    placeholder="description"
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                />
+                <input
+                    type="text"
+                    placeholder="amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={()=> false}
+                />
+                <textarea
+                    placeholder="Add a not for your expense (optional)"
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                />
+                <button>Add Expanse</button>
+            </form>
+        )
+    }
+}
